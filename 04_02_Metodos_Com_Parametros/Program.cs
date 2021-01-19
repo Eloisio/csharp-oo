737b05b3ff852adaaf9fda42498e961641a88d86
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:00:37 -0300

    04.02 - Métodos com parâmetros

---
+++ 04_02_Metodos_Com_Parametros/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Contas;
+
+namespace _04_02_Metodos_Com_Parametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta(); 
+            
+            conta.Numero = "001";
+            conta.DataAbertura = DateTime.Today;
+            conta.Saldo = 100;
+
+            conta.ExibirExtrato();
+            conta.Depositar(50);
+            conta.ExibirExtrato();
+            conta.Sacar(30);
+            conta.ExibirExtrato();
+        }
+    }
+}
