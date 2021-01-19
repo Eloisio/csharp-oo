02a685f9d7d7ed779eb70e0187899970a832f6ef
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:10:03 -0300

    04.06 - Utilizando construtores

---
+++ 04_06_Utilizando_Construtores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Contas;
+
+namespace _04_06_Utilizando_Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta();             
+            Console.WriteLine(conta.ExibirExtrato());
+
+            var conta2 = new Conta("0001", DateTime.Now, 100);             
+            Console.WriteLine(conta2.ExibirExtrato());
+        }
+    }
+}
