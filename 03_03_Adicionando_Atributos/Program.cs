207ef423ca359a6814a1a3b8f4ca3eaa9066b0a0
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 21:58:47 -0300

    03.03 - Adicionando atributos à classe

---
+++ 03_03_Adicionando_Atributos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Contas;
+
+namespace _03_03_Adicionando_Atributos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta(); 
+            
+            conta.Numero = "001";
+            conta.DataAbertura = DateTime.Today;
+            conta.Saldo = 100;
+
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(conta.DataAbertura);
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
