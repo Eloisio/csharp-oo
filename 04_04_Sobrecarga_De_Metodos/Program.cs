9469b2426b9421c3be599a6747962e3fea860ae2
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:09:02 -0300

    04.04 - Sobrecarga de métodos

---
+++ 04_04_Sobrecarga_De_Metodos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Contas;
+
+namespace _04_04_Sobrecarga_De_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta(); 
+            
+            conta.Numero = "001";
+            conta.DataAbertura = DateTime.Today;
+            conta.Saldo = 100;
+            conta.RealizarEmprestimo(200, 10);
+            Console.WriteLine(conta.ExibirExtrato());
+        }
+    }
+}
