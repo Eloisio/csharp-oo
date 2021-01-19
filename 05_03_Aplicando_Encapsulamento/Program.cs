bc9f57056d60f43e815161a6d353d15820446893
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:11:46 -0300

    05.03 - Aplicando encapsulamento

---
+++ 05_03_Aplicando_Encapsulamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RH;
+
+namespace _05_03_Aplicando_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario = new Funcionario("Joel", "000.000.000-00");
+            Console.WriteLine(funcionario.GetCPF());
+            funcionario.SetCPF("");
+            Console.WriteLine(funcionario.GetCPF());
+        }
+    }
+}
