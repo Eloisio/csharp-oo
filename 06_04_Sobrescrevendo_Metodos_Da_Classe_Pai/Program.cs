699a5dc5ef97654c9aca6828bad6fa17a1a7138a
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:14:42 -0300

    06.04 - Sobrescrevendo métodos da classe pai

---
+++ 06_04_Sobrescrevendo_Metodos_Da_Classe_Pai/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Personagens;
+
+namespace _06_04_Sobrescrevendo_Metodos_Da_Classe_Pai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cavaleiro = new Cavaleiro("Aragorn", 100);
+            var arqueiro = new Arqueiro("Legolas", 100);
+            var mago = new Mago("Gandalf", 100, 100);
+
+            cavaleiro.Atacar();
+            cavaleiro.Defender();
+
+            arqueiro.Atacar();
+            arqueiro.Defender();
+            
+            mago.Atacar();
+            mago.Defender();
+        }
+    }
+}
