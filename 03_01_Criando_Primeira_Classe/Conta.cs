011a67ef200d544913dcb32ee8675ea5c3151e6f
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 21:51:43 -0300

    03.01 - Criando nossa primeira classe

---
+++ 03_01_Criando_Primeira_Classe/Conta.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _03_01_Criando_Primeira_Classe
+{
+    public class Conta
+    {
+        public string Numero;
+        public DateTime DataAbertura;
+        public decimal Saldo;
+    }
+}
---
+++ 03_01_Criando_Primeira_Classe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_01_Criando_Primeira_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
