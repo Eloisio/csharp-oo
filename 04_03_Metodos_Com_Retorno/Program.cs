2c0596f538b74383dd6bf12cd6f18c8ede5c0c1c
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:07:25 -0300

    04.03 - Métodos com retorno

---
+++ 04_03_Metodos_Com_Retorno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Contas;
+
+namespace _04_03_Metodos_Com_Retorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta(); 
+            
+            conta.Numero = "001";
+            conta.DataAbertura = DateTime.Today;
+            conta.Saldo = 100;
+
+            Console.WriteLine(conta.ExibirExtrato());
+            conta.Depositar(50);
+            Console.WriteLine(conta.ExibirExtrato());
+            conta.Sacar(30);
+            Console.WriteLine(conta.ExibirExtrato());
+        }
+    }
+}
