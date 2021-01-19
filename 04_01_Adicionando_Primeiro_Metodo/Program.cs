9bf95119bf80668bf0763f947d8150122a10f9b9
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 21:59:46 -0300

    04.01 - Adicionando o primeiro método

---
+++ 04_01_Adicionando_Primeiro_Metodo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Contas;
+
+namespace _04_01_Adicionando_Primeiro_Metodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta(); 
+            
+            conta.Numero = "001";
+            conta.DataAbertura = DateTime.Today;
+            conta.Saldo = 100;
+
+            conta.ExibirExtrato();
+        }
+    }
+}
