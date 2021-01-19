b6dc104e8522a77aa01644905a794d6380de242c
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 21:57:55 -0300

    03.02 - Como criar um objeto

---
+++ 03_02_Como_Criar_Objetos/Conta.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Contas
+{
+    public class Conta
+    {
+        
+    }
+}
---
+++ 03_02_Como_Criar_Objetos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Contas;
+
+namespace _03_02_Como_Criar_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new Conta(); 
+            Console.WriteLine(conta);
+        }
+    }
+}
