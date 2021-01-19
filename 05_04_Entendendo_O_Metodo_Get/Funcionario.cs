e98d2f7b6a4d3ebe58d59a1ffad7d93edfacb71a
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:12:11 -0300

    05.04 - Entendendo o método get

---
+++ 05_04_Entendendo_O_Metodo_Get/Funcionario.cs
@@ -0,0 +1,22 @@
+namespace RH
+{
+    public class Funcionario
+    {
+        public string Nome;
+        private string cpf;
+        public string CPF
+        {
+            get
+            {
+                return cpf.Replace(".", "").Replace("-","");
+            }
+        }
+
+
+        public Funcionario(string nome, string _cpf)
+        {
+            Nome = nome;
+            cpf = _cpf;
+        }
+    }
+}
---
+++ 05_04_Entendendo_O_Metodo_Get/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RH;
+
+namespace _05_04_Entendendo_O_Metodo_Get
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario = new Funcionario("Joel", "000.000.000-00");
+            Console.WriteLine(funcionario.CPF);
+        }
+    }
+}
