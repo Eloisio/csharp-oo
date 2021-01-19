a6afb52969a56d797e0a0a388c31b72260f153e3
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-19 01:05:34 -0300

    08.02 - Criando nossa primeira interface

---
+++ 08_02_Criando_Nossa_Primeira_Interface/Itens/Joia.cs
@@ -0,0 +1,12 @@
+namespace Itens
+{
+    public class Joia : Item
+    {
+        public int Valor { get; set; }
+
+        public Joia(string nome, int x, int y, int valor) : base(nome, x, y)
+        {
+            Valor = valor;    
+        }
+    }
+}
---
+++ 08_02_Criando_Nossa_Primeira_Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08_02_Criando_Nossa_Primeira_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
