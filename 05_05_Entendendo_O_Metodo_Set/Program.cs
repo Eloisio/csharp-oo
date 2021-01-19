991a0c43a81fd878f8d210b1965f1a715d8ae04f
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-18 22:12:37 -0300

    05.05 - Entendendo o método set

---
+++ 05_05_Entendendo_O_Metodo_Set/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RH;
+
+namespace _05_05_Entendendo_O_Metodo_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario = new Funcionario("Joel", "000.000.000-00");
+            funcionario.CPF = "";
+            Console.WriteLine(funcionario.CPF);
+        }
+    }
+}
