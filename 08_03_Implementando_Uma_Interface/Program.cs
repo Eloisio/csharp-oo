371aa0beb447beef3926893c1e099c142304a8e1
Joel Rodrigues <joel.lima@engenhos.net>
2021-01-19 01:05:59 -0300

    08.03 - Implementando uma interface

---
+++ 08_03_Implementando_Uma_Interface/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Itens;
+
+namespace _08_03_Implementando_Uma_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var espada = new Espada("Excalibur", 10, 10, 100);
+            espada.Colecionar();
+
+            var pocao = new Pocao("Poção de Vida", 20, 20, 50);
+            pocao.Consumir();
+
+            var joia = new Joia("Diamante", 30, 30, 100);
+            joia.Colecionar();
+            joia.Consumir();
+        }
+    }
+}
